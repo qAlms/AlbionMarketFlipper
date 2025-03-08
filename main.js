e3235fcef23113a8a878133470b95445963ba3c1
qAlms <106905611+qAlms@users.noreply.github.com>
2025-03-08 18:10:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ async function fetchMarketData() {
 
 const balls = document.getElementById("btn1");
 
-balls.addEventListener("click", fetchMarketData());
+balls.addEventListener("submit", fetchMarketData());
 
 function displayData(data, profit, minCity, minSell, maxCity, maxSell, taxRate, setupFee) {
     const table = document.getElementById("marketTable");
